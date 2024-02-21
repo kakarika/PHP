44943665ac18198145cff4690d3679b1385ef42e
pyaesone21 <pyaesone.puppey@gmail.com>
2024-02-21 22:02:34 +0630

    no message

---
+++ day3/day3.php
@@ -0,0 +1,151 @@
+<?php
+
+function testInput($data)
+{
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
+
+$nameErr = $emailErr = $genderErr = $websiteErr = $passwordErr = $confirmPasswordErr = "";
+$name = $email = $comment = $gender = $website = "";
+
+$formSubmitted = false;
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (empty($_POST["name"])) {
+        $nameErr = "*Name is required";
+    } else {
+        $name = testInput($_POST["name"]);
+        if (!preg_match("/^[a-zA-Z- ']*$/", $name)) {
+            $nameErr = "*Only letters and white space allowed";
+        }
+    }
+    if (empty($_POST["email"])) {
+        $emailErr = "Email is required";
+    } else {
+        $email = testInput($_POST["email"]);
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            $emailErr = "*Invalid email format";
+        }
+    }
+    if (empty($_POST["email"])) {
+        // $commentErr = "";
+    } else {
+        $comment = $_POST["comment"];
+    }
+    if (empty($_POST["gender"])) {
+        $genderErr = "*Gender is required";
+    } else {
+        $gender = testInput($_POST["gender"]);
+    }
+    if (empty($_POST["website"])) {
+        $websiteErr = "";
+    } else {
+        $website = testInput($_POST["website"]);
+        if (!preg_match("/\b(?:(?:https?|ftp):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i", $website)) {
+            $websiteErr = "*Invalid URL";
+        }
+    }
+    if (empty($_POST["password"])) {
+        $passwordErr = "Password is required";
+    } else {
+        if ((preg_match("/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$/", $_POST["password"]) === 0)) {
+            $passwordErr = "*Password must be at least 8 characters and must contain at least one lower case letter, one upper case letter and one digit";
+        } else {
+            $password = $_POST["password"];
+        }
+    }
+    if (empty($_POST["confirmPassword"])) {
+        $confirmPasswordErr = "Confirm password is required";
+    } else {
+        if ($_POST["confirmPassword"] !== $_POST["password"]) {
+            $confirmPasswordErr = "*Password doesn't match";
+        }
+    }
+
+    if (empty($nameErr) && empty($emailErr) && empty($genderErr) && empty($websiteErr) && empty($passwordErr) && empty($confirmPasswordErr)) {
+        $formSubmitted = true;
+    }
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <style>
+        .form-control {
+            border-color: blueviolet;
+        }
+    </style>
+</head>
+
+<body>
+    <h1 class="text-center my-5">PHP Form Validation Example</h1>
+    <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" class="">
+        <div class="w-25 m-auto">
+            <div class="form-group">
+                <label for="name">Name:</label>
+                <input type="text" name="name" id="name" class="form-control mb-2" value="<?php echo $name; ?>">
+                <span class="error text-danger"><?php echo $nameErr; ?></span>
+            </div>
+            <div class="form-group">
+                <label for="email">E-mail:</label>
+                <input type="email" name="email" id="email" class="form-control mb-2" value="<?php echo $email; ?>">
+                <span class="error text-danger"><?php echo $emailErr; ?></span>
+            </div>
+            <div class="form-group">
+                <label for="website">Website:</label>
+                <input type="text" name="website" id="website" class="form-control mb-2" value="<?php echo $website; ?>">
+                <span class="error text-danger"><?php echo $websiteErr; ?></span>
+            </div>
+            <div class="form-group">
+                <label for="comment">Comment:</label>
+                <textarea name="comment" id="comment" cols="40" rows="5" class="form-control mb-2"><?php echo $comment; ?></textarea>
+            </div>
+            <div class="form-group">
+                <label for="gender">Gender:</label>
+                <div>
+                    <input type="radio" id="male" name="gender" value="male" <?php if (isset($gender) && $gender == "male") echo "checked"; ?>>
+                    <label for="male">Male</label>
+                </div>
+                <div>
+                    <input type="radio" id="female" name="gender" value="female" <?php if (isset($gender) && $gender == "female") echo "checked"; ?>>
+                    <label for="female">Female</label>
+                </div>
+                <div>
+                    <input type="radio" id="other" name="gender" value="other" <?php if (isset($gender) && $gender == "other") echo "checked"; ?>>
+                    <label for="other">Other</label>
+                </div>
+                <span class="error text-danger"><?php echo $genderErr; ?></span>
+            </div>
+            <div class="form-group">
+                <label for="password">Password:</label>
+                <input type="password" name="password" id="password" class="form-control mb-2">
+                <span class="error text-danger"><?php echo $passwordErr; ?></span>
+            </div>
+            <div class="form-group">
+                <label for="confirmPassword">Confirm Password:</label>
+                <input type="password" name="confirmPassword" id="confirmPassword" class="form-control mb-2">
+                <span class="error text-danger"><?php echo $confirmPasswordErr; ?></span>
+            </div>
+            <div class="text-center">
+                <input type="submit" name="submit" value="Submit" class="btn btn-info mt-2 w-50">
+            </div>
+            <?php if ($formSubmitted) { ?>
+                <div class="alert alert-success" role="alert">
+                    Form submitted successfully!
+                </div>
+            <?php } ?>
+        </div>
+    </form>
+</body>
+
+</html>
